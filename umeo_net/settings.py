41e0c9d8264da267e0044c7c531f927a3da476c1
Ubuntu <ubuntu@ip-172-31-37-59.ap-northeast-1.compute.internal>
2021-01-24 15:04:53 +0000

    aws側　debugとか変えた

--- umeo_net/settings.py
+++ umeo_net/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'frw9r)thr&+@wd7q^p+docs*(q)ya&^_0l#e=q%a&ygz#m@h8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['18.183.181.195']
 
