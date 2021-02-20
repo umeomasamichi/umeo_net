183366f6d0cbc9f194cc884822c88a73c46bc01c
Ubuntu <ubuntu@ip-172-31-37-59.ap-northeast-1.compute.internal>
2021-02-20 08:18:08 +0000

    test5

--- auto.sh
+++ auto.sh
@@ -4,9 +4,14 @@ git add -A
 read COM
 git commit -m $COM
 git pull --no-edit
+echo "git pull completed!"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+echo "migration completed!"
+python manage.py collectstatic --noinput
+echo "collect completed!"
 sudo systemctl restart nginx.service
+echo "nginx restarted!"
 pkill gunicorn 
-gunicorn --bind 127.0.0.1:8000 umeo_net.wsgi -D 
+gunicorn --bind 127.0.0.1:8000 umeo_net.wsgi -D
+echo "gunicorn restarted!"
